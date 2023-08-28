cf8ae8635ac659e7eb3eb2c63de88a37138dd015
Donghwan Shin <d.shin@sheffield.ac.uk>
2023-08-28 18:52:08 +0000

    Fix setup_carla.sh
    Fix: unziped contents are correctly moved into Carla_Versions

--- setup_carla.sh
+++ setup_carla.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
+# download customised CARLA
 wget https://figshare.com/ndownloader/files/30458664
 wget https://figshare.com/ndownloader/files/30458646
 wget https://figshare.com/ndownloader/files/30457506
-unzip 30458664
-unzip 30458646
-unzip 30457506
+
+# make a target folder
 mkdir Carla_Versions
-mv 30458664 Carla_Versions
-mv 30458646 Carla_Versions
-mv 30457506 Carla_Versions
+
+# unzip files into the target folder
+unzip 30458664 -d Carla_Versions
+unzip 30458646 -d Carla_Versions
+unzip 30457506 -d Carla_Versions
+
+# remove zip files
+rm 30458664 30458646 30457506
